6ef042a44844e14aa600f0e090666f81d46de272
andresaugusto <andres.augusto@icloud.com>
2024-11-24 15:20:36 -0400

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      // hostname: 's3.us-east-2.amazonaws.com',
+      hostname: 'andresaugusto-aa-media.s3.amazonaws.com',
+      port: '',
+      pathname: '/images/**'
+    }]
+  },
 };
 
 export default nextConfig;
